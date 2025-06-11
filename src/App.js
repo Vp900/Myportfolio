8e144bb5aec2fb233e13ac4413743bc6c7d053bb
Vikas Pal <vikaspal9004@gmail.com>
2025-06-11 17:15:16 +0530

    Updated stats section styling and responsiveness

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import Header from './components/Header';
 import Hero from './components/Hero';
+import Stats from './components/Stats';
 
 function App() {
   return (
     <>
       <Header />
       <Hero/>
-    
+      <Stats />
     </>
   );
 }
